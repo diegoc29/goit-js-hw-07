60ae3bffe82e081ca8c3e6cee0e8f3fd8f50683e
diego cuervo <dicuervo213@gmail.com>
2023-07-11 14:55:43 -0500

    actualizacion index js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,55 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+let lightboxInstance = null; 
+
+const markup = galleryItems
+  .map(
+    (item) => `
+    <li>
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    </li>
+  `
+  )
+  .join('');
+
+galleryContainer.innerHTML = markup;
+
+galleryContainer.addEventListener('click', function (event) {
+    event.preventDefault();
+
+
+if (event.target.tagName === 'IMG') {
+    const imageUrl = event.target.parentElement.href;    
+  
+
+ lightboxInstance = basicLightbox.create(`
+      <img src="${imageUrl}" alt="Imagen en ventana modal" />
+    `, {
+      onShow: (instance) => {
+        const modalImage = instance.element().querySelector('img');
+      
+        modalImage.src = imageUrl;
+
+        document.addEventListener('keyup', handleKeyUb);
+      },
+
+      onClose: () => {
+        document.removeEventListener('keyup', handleKeyUb);
+      }
+    });
+    
+    lightboxInstance.show();
+  }
+});
+
+function handleKeyUb(event){
+  if (event.key === 'Escape') {
+    lightboxInstance.close();
+  }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,63 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+document.addEventListener('DOMContentLoaded', function () {
+  new SimpleLightbox ('.sl-gallery a',{
+    captionsData: "alt",
+    captionDelay: 250
+  });
+});
+
+const galleryContainer = document.querySelector('.gallery');
+let lightboxInstance = null; 
+
+
+const markup = galleryItems
+  .map(
+    (item) => `
+    <li>
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    </li>
+  `
+  )
+  .join('');
+
+galleryContainer.innerHTML = markup;
+
+galleryContainer.addEventListener('click', function (event) {
+    event.preventDefault();
+
+
+if (event.target.tagName === 'IMG') {
+    const imageUrl = event.target.parentElement.href;    
+  
+
+ lightboxInstance = SimpleLightbox.create(`
+      <img src="${imageUrl}" alt="Imagen en ventana modal" />
+    `, {
+      onShow: (instance) => {
+        const modalImage = instance.element().querySelector('img');
+      
+        modalImage.src = imageUrl;
+
+        document.addEventListener('keyup', handleKeyUb);
+      },
+
+      onClose: () => {
+        document.removeEventListener('keyup', handleKeyUb);
+      }
+    });
+    
+    lightboxInstance.show();
+  }
+});
+
+function handleKeyUb(event){
+  if (event.key === 'Escape') {
+    lightboxInstance.close();
+  }
+}
+
